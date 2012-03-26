1e41c45de29525e4f5295028a0960ed25a65263e
Jacobo de Vera <devel@jacobodevera.com>
2012-03-26 10:12:25 +0100

    Add module to install my version of Bashmarks
    I have a customised version of bashmarks at jdevera/bashmarks. This
    module clones the repository and links the main library in the .bash.d
    directory created by the dotfiles installer, so that bashmarks functions
    are sourced when the shell starts.

---
+++ modules/052_bashmarks.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+MODULE_NAME='Install Bashmarks'
+
+log_module_start
+
+REPO_URL='https://github.com/jdevera/bashmarks'
+DEST_DIR="$HOME/other/run/share/lib/bashmarks"
+
+is_file "$DEST_DIR/bashmarks.sh" && log_no_changes
+
+git clone "$REPO_URL" "$DEST_DIR"
+
+# Quick sanity check
+source "$DEST_DIR/bashmarks.sh"
+
+ln -s "$DEST_DIR/bashmarks.sh" "$HOME/.bash.d/local/after/bashmarks.sh"
+
+log_module_end
