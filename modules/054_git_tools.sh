33e7d4a68775ec3448c7f48a9c34b6d5023de0a4
Jacobo de Vera <devel@jacobodevera.com>
2012-03-28 14:39:10 +0100

    Install hub standalone from sources
    Get hub from its github repo rather than the author's website, since it
    has shown to be unreliable.
    
    The main drawback is that now I need to install rake, the main advantage
    is that I get Bash completion for free.
    
    Also make git be an alias for hub.
    
    Fixes #6

--- modules/054_git_tools.sh
+++ modules/054_git_tools.sh
@@ -4,7 +4,10 @@ MODULE_NAME='Git tools'
 
 log_module_start
 
-BIN_DIR="$HOME/other/run/bin"
+PREFIX="$HOME/other/run"
+BIN_DIR="$PREFIX/bin"
+LIB_DIR="$PREFIX/lib"
+LOCAL_BASH_DIR="$HOME/.bash.d/local/after"
 
 changes=false
 
@@ -21,10 +24,15 @@ if ! is_exec "$BIN_DIR/hub"
 then
     log_info "Installing hub" | indent
     program_is_available ruby || package_install ruby1.9.1
-    download_file_to "http://defunkt.io/hub/standalone" "/tmp/"
-    mv /tmp/standalone $BIN_DIR/hub
-    chmod +x $BIN_DIR/hub
+    program_is_available rake || package_install rake
+    is_dir "$LIB_DIR/hub" && remove_dir "$LIB_DIR/hub"
+    git clone git://github.com/defunkt/hub.git "$LIB_DIR/hub"
+    cd "$LIB_DIR/hub"
+    rake install prefix="$PREFIX"
     is_exec $BIN_DIR/hub && changes=true
+    # Install bash completion
+    ln -sf "$LIB_DIR/hub/etc/hub.bash_completion.sh" "$LOCAL_BASH_DIR/"
+    $BIN_DIR/hub alias -s > "$LOCAL_BASH_DIR/hub.alias.sh"
 fi
 
 $changes || log_no_changes
