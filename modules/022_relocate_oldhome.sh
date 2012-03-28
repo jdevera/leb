5a3fa01c92b3c87ffd20281ea765ca00d267e65c
Jacobo de Vera <devel@jacobodevera.com>
2012-03-28 14:43:34 +0100

    Always use a timestamped dir for old home backups
    Store old home backups in a timestamped directory under backup/oldhome
    
    Fixes #7

--- modules/022_relocate_oldhome.sh
+++ modules/022_relocate_oldhome.sh
@@ -4,13 +4,11 @@ MODULE_NAME='Old home directory backup relocation'
 log_module_start
 
 HOME_BACKUP_DIR="$HOME/.oldhome"
-HOME_BACKUP_PROPER_DIR="$HOME/backup/oldhome"
+HOME_BACKUP_PROPER_DIR="$HOME/backup/oldhome/$(date +%Y%M%d%_H%M%S)"
 
 
 is_dir $HOME_BACKUP_DIR || log_no_changes
 
-is_dir $HOME_BACKUP_PROPER_DIR && \
-	HOME_BACKUP_PROPER_DIR="${HOME_BACKUP_PROPER_DIR}_$(date +%Y%M%d%H%M%S)"
 mv $HOME_BACKUP_DIR $HOME_BACKUP_PROPER_DIR
 
 log_module_end
