6be37f7f116e4e4349d4b0e44c3bd8149b0d8911
Jacobo de Vera <devel@jacobodevera.com>
2012-04-27 17:23:03 +0100

    Programmatically choose the sudoers group
    When setting up passwordless sudo, Ubuntu used to use the admin group
    but since 12.04 it has switched to the sudo group, so figure out which
    one to use based on which one the user running LEB belongs to.

--- modules/001_pwless_sudo.sh
+++ modules/001_pwless_sudo.sh
@@ -4,12 +4,14 @@ MODULE_NAME='Passwordless sudo'
 
 log_module_start
 
-NOPASS_FILE='/etc/sudoers.d/admin_nopass'
+NOPASS_FILE='/etc/sudoers.d/sudoers_nopass'
 
 is_file $NOPASS_FILE && log_no_changes
 
+id -Gn | grep -q '\bsudo\b' && sudoers_group=sudo
+id -Gn | grep -q '\badmin\b' && sudoers_group=admin
 
-echo '%admin ALL=(ALL) NOPASSWD: ALL' | sudo tee /tmp/nopass_file > /dev/null
+echo "%$sudoers_group ALL=(ALL) NOPASSWD: ALL" | sudo tee /tmp/nopass_file > /dev/null
 sudo chmod 0440 /tmp/nopass_file
 sudo mv /tmp/nopass_file $NOPASS_FILE
 
