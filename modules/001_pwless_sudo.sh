ed75e261d65cbc91828f8ff0a0395751c5209edf
Jacobo de Vera <jacobo.devera@qnective.com>
2014-03-12 13:22:47 +0100

    In passwordless sudo, allow for user not in groups
    Passwordless sudo worked based on admin and sudo groups. But if the
    current user is not in either of those groups, we still want to setup
    passwordless sudo. Do so based on the user name in that case.

--- modules/001_pwless_sudo.sh
+++ modules/001_pwless_sudo.sh
@@ -8,10 +8,24 @@ NOPASS_FILE='/etc/sudoers.d/sudoers_nopass'
 
 is_file $NOPASS_FILE && log_no_changes
 
-id -Gn | grep -q '\bsudo\b' && sudoers_group=sudo
-id -Gn | grep -q '\badmin\b' && sudoers_group=admin
+function sudoers_group()
+{
+    local userinfo="$(id -Gn)"
+    local group
+    if echo "$userinfo" | grep -q '\bsudo\b'
+    then
+        echo '%sudo'
+        return
+    elif echo "$userinfo" | grep -q '\badmin\b'
+    then
+        echo '%admin'
+        return
+    else
+        id -un
+    fi
+}
 
-echo "%$sudoers_group ALL=(ALL) NOPASSWD: ALL" | sudo tee /tmp/nopass_file > /dev/null
+echo "$(sudoers_group) ALL=(ALL) NOPASSWD: ALL" | sudo tee /tmp/nopass_file > /dev/null
 sudo chmod 0440 /tmp/nopass_file
 sudo mv /tmp/nopass_file $NOPASS_FILE
 
