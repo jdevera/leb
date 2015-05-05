8a773a64f16b17ed0b8206acdb7585d881e39331
Jacobo de Vera <devel@jacobodevera.com>
2015-05-05 22:46:55 +0200

    Disable PPAs for git and vim
    They don't seem to be updated for Ubuntu 15.04

--- modules/010_ppas.sh
+++ modules/010_ppas.sh
@@ -4,7 +4,7 @@ MODULE_NAME='Add PPAs'
 
 
 declare -A PPAS
-PPAS['ppa:nmi/vim-snapshots']='Vim Snapshots'
-PPAS['ppa:git-core/ppa']='Git'
+# PPAS['ppa:nmi/vim-snapshots']='Vim Snapshots'
+# PPAS['ppa:git-core/ppa']='Git'
 
 module_ppas
