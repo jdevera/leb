73d3f6934971f33a3d339200de4fcb769bf08b19
Jacobo de Vera <devel@jacobodevera.com>
2015-05-05 22:50:56 +0200

    Don't try to move an empty HOME backup

--- modules/022_relocate_oldhome.sh
+++ modules/022_relocate_oldhome.sh
@@ -7,9 +7,10 @@ HOME_BACKUP_DIR="$HOME/.oldhome"
 HOME_BACKUP_PROPER_DIR="$HOME/backup/oldhome/$(date +%Y%M%d%_H%M%S)"
 
 
-is_dir $HOME_BACKUP_DIR || log_no_changes
+is_dir "$HOME_BACKUP_DIR" || log_no_changes
+dir_is_empty "$HOME_BACKUP_DIR" && log_no_changes
 
-mv $HOME_BACKUP_DIR $HOME_BACKUP_PROPER_DIR
+mv "$HOME_BACKUP_DIR" "$HOME_BACKUP_PROPER_DIR"
 
 log_module_end
 
