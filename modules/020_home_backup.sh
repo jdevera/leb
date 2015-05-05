3d13b0765f599616cb19534a7ff94d74efcbb802
Jacobo de Vera <devel@jacobodevera.com>
2015-05-05 22:48:29 +0200

    Don't try to back up an empty HOME
    Also, remove stupid tabs.

--- modules/020_home_backup.sh
+++ modules/020_home_backup.sh
@@ -8,22 +8,29 @@ HOME_BACKUP_DIR="$HOME/.oldhome"
 
 function has_all_home_dirs()
 {
-	local directories='backup comms devel doc media other'
-	for dir in $directories
-	do
-		is_dir "$HOME/$dir" || return 1
-	done
-	return 0
+    if dir_is_empty "$HOME"
+    then
+        echo "Home directory is empty, no backup performed"
+        return 1
+    fi
+
+    local directories='backup comms devel doc media other'
+    for dir in $directories
+    do
+        is_dir "$HOME/$dir" || return 1
+    done
+
+    return 0
 }
 
 function backup_home()
 {
-	create_dir "$HOME_BACKUP_DIR"
-	mv $HOME/* $HOME_BACKUP_DIR/
+    create_dir "$HOME_BACKUP_DIR"
+    mv $HOME/* $HOME_BACKUP_DIR/
 }
 
 
-# If we already have the good structure, do nothing
+# If we already have the good structure or home is empty, do nothing
 has_all_home_dirs && log_no_changes
 
 backup_home
