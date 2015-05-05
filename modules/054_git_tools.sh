355f06615201284ce1172203acd7da962012ffd7
Jacobo de Vera <devel@jacobodevera.com>
2015-05-05 22:43:58 +0200

    Update hub installation to new binary distribution
    Hub is now written in Go and binary releases are provided on github.

--- modules/054_git_tools.sh
+++ modules/054_git_tools.sh
@@ -10,6 +10,8 @@ LOCAL_BASH_DIR="$HOME/.bash.d/local/after"
 
 changes=false
 
+# git-wtf
+
 if ! is_exec "$USER_BIN_DIR/git-wtf"
 then
     log_info "Installing git-wtf"
@@ -19,18 +21,54 @@ then
     is_exec $USER_BIN_DIR/git-wtf && changes=true
 fi
 
+
+install_hub()
+{
+    local plat='386'
+    [[ $(getconf LONG_BIT) == '64' ]] && plat='amd64'
+    local url="$(curl -s https://api.github.com/repos/github/hub/releases/latest |
+        grep "download.*linux-${plat}" | sed 's/.*"\(https:.*\)"/\1/')"
+    local filename="$(basename "$url")"
+    local hubdir="$USER_SHARE_LIB_DIR/hub"
+    local distpath="$hubdir/$filename"
+
+    # Get the tarball
+    if ! is_file "$distpath"
+    then
+        create_dir "$hubdir"
+        log_info "Hub: Downloading distribution"
+        download_file_to "$url" "$hubdir"
+    fi
+    if ! is_file "$distpath"
+    then 
+        log_fatal "Hub: Could not download"
+    fi
+
+    # Now it is downloaded, untar
+    log_info "Hub: Unpacking"
+    local dirname="$(tar tf "$distpath" | head -1)"
+    local dirpath="$hubdir/$dirname"
+    tar -C "$hubdir" -x -f "$distpath"
+
+    # Make links
+    log_info "Hub: Setting up"
+    local binpath="$USER_BIN_DIR/hub"
+    rm -f "$binpath"
+
+    command ln -s "$dirpath/hub" "$binpath"
+
+    # Install bash completion
+    command ln -s "$dirpath/etc/hub.bash_completion.sh" "$LOCAL_BASH_DIR/"
+    "$binpath" alias -s > "$LOCAL_BASH_DIR/hub.alias.sh"
+
+}
+
+
 if ! is_exec "$USER_BIN_DIR/hub"
 then
     log_info "Installing hub"
-    program_is_available rake || package_install rake
-    is_dir "$USER_SHARE_LIB_DIR/hub" && remove_dir "$USER_SHARE_LIB_DIR/hub"
-    git clone git://github.com/defunkt/hub.git "$USER_SHARE_LIB_DIR/hub"
-    cd "$USER_SHARE_LIB_DIR/hub"
-    rake install prefix="$USER_PREFIX_DIR"
+    install_hub
     is_exec $USER_BIN_DIR/hub && changes=true
-    # Install bash completion
-    ln -sf "$USER_SHARE_LIB_DIR/hub/etc/hub.bash_completion.sh" "$LOCAL_BASH_DIR/"
-    $USER_BIN_DIR/hub alias -s > "$LOCAL_BASH_DIR/hub.alias.sh"
 fi
 
 $changes || log_no_changes
