b4904b0ec1d034b64fcf075dd5d86fc7570e8081
Jacobo de Vera <devel@jacobodevera.com>
2019-03-07 16:58:45 +0100

    Install fonts only if X installed

--- modules/packaged_fonts.sh
+++ modules/packaged_fonts.sh
@@ -2,4 +2,12 @@
 
 MODULE_NAME='Packaged fonts'
 
-module_packages xfonts-terminus
+if distro_has_gui
+then
+    module_packages xfonts-terminus
+else
+    log_module_start
+    log_info "GUI not available, skipping fonts installation"
+    log_no_changes
+fi
+
