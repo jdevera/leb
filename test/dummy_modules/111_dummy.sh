dc97ed7c626fb17aa2458f292f3cfa64f1d5cf19
Jacobo de Vera <devel@jacobodevera.com>
2020-03-22 23:38:32 +0000

    Test: Add dummy modules for testing the -d option

---
+++ test/dummy_modules/111_dummy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+MODULE_NAME='Dummy module'
+
+log_module_start
+# log_no_changes
+
+log_info "blah"
+log_info "blah"
+log_info "blah"
+log_info "blah"
+log_info "blah"
+log_module_end
---
+++ test/dummy_modules/112_dummy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+MODULE_NAME='Dummy module'
+
+log_module_start
+log_no_changes
+
+log_module_end
